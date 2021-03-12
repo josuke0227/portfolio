34b2c0c6b44c024d767ad5922f6e5b6343d0982b
Josuke <y.motosugi0227@gmail.com>
2021-03-12 23:52:48 +1100

    Fix skill stack img bug

--- components/SkillStack.js
+++ components/SkillStack.js
@@ -7,25 +7,42 @@ export default function SkillStack() {
       <div>
         <h2>Skill stack</h2>
         <div className={style.skillStackShowcase}>
-          <img className={style.square} src="/HTML5_Logo.svg" />
-          <img className={style.squareCss} src="/css_logo.svg" />
-          <img className={style.square} src="/javascript_logo.png" />
-          <img className={style.square} src="/sass_logo.svg" />
-          <img className={style.squareReact} src="/react_logo.svg" />
-          <img className={style.square} src="/react_bootstrap_logo.png" />
-          <img className={style.square} src="/styled-components_logo.png" />
-          <img className={style.square} src="/next_logo.png" />
           <img
-            className={style.twoSpans1}
-            src="/nodejs_logo.svg"
-            width="200"
-            height="80"
+            className={`${style.square} ${style.html}`}
+            src="/HTML5_Logo.svg"
+          />
+          <img className={`${style.square} ${style.css}`} src="/css_logo.svg" />
+          <img
+            className={`${style.square} ${style.js}`}
+            src="/javascript_logo.png"
           />
           <img
-            className={style.twoSpans2}
+            className={`${style.square} ${style.next}`}
+            src="/next_logo.png"
+          />
+          <img
+            className={`${style.square} ${style.sass}`}
+            src="/sass_logo.svg"
+          />
+          <img
+            className={`${style.square} ${style.react}`}
+            src="/react_logo.svg"
+          />
+          <img
+            className={`${style.square} ${style.node}`}
+            src="/nodejs_logo.svg"
+          />
+          <img
+            className={`${style.square} ${style.reactBootstrap}`}
+            src="/react_bootstrap_logo.png"
+          />
+          <img
+            className={`${style.square} ${style.styledComponents}`}
+            src="/styled-components_logo.png"
+          />
+          <img
+            className={`${style.square} ${style.mongoDb}`}
             src="/mongodb_logo.png"
-            width="200"
-            height="100"
           />
         </div>
         <SkillEvidences />
