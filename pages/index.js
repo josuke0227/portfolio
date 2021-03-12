00fddae66d6ef79630d5919c5e4b7a164f46a320
Josuke <y.motosugi0227@gmail.com>
2021-03-13 00:08:35 +1100

    test

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import MainContents from "../components/MainContents";
 import NavBar from "../components/NavBar";
-import "../styles/normalize.css";
 
 export default function Home() {
   return (
     <>
       <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Yosuke Motosugi - Frontend developer</title>
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
